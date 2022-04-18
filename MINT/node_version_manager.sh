6abc183f9d230cc5f5f0c6388862f84783257d0c
joseguilherme-dev <joseguilhermefmoura@gmail.com>
2022-04-18 15:59:59 -0300

    :bulb: Add node_version_manager download script

---
+++ MINT/node_version_manager.sh
@@ -0,0 +1,11 @@
+####################################################################################################
+#                                                                                                  #
+# This shell script downloads the latest stable version of Node Version Manager on LINUX MINT.     #
+#                                                                                                  #
+# ##################################################################################################
+
+# Download the Node Version Manager
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# You can use it to install the latest version of Node, by executing `nvm install node` in a new terminal.
+
